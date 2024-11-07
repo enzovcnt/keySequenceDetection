0c4be7b99cdeb4fe8a38c3fd9d56413f961df36b
enzo <enzovcnt@gmail.com>
2024-11-08 00:12:12 +0100

    the word "bonjour" appear in the console log with a key sequence.

--- js/main.js
+++ js/main.js
@@ -1,1 +1,12 @@
-alert("Hello World!");
+const pressed = [];
+const secretCode = 'enzo';
+
+window.addEventListener('keyup', (e) => {
+    console.log(e.key);
+    pressed.push(e.key);
+    pressed.splice(-secretCode.length -1, pressed.length - secretCode.length);
+    if(pressed.join('').includes(secretCode)) {
+        console.log('bonjour');
+    }
+    console.log(pressed);
+})
